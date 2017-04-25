2aa5fbbe28036d11c1d29559ce74a319c9ad3bd3
Cody Pearce <codyapearce@gmail.com>
2017-04-25 08:28:04 -0600

    add expense function

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
 var app = angular.module('budgetApp', []);
 
 app.controller('budgetCtrl', function($scope) {
-    
+    $scope.expenses = [];
+
+    $scope.addExpense = function(expense) {
+      console.log(expense)
+      $scope.expenses.push(expense);
+    }
 })
