45c74b2ad3b11b306271e0fb5fd8430244134e47
Cody Pearce <codyapearce@gmail.com>
2017-04-26 19:45:22 -0600

    expense total

--- app/app.js
+++ app/app.js
@@ -2,9 +2,10 @@ var app = angular.module('budgetApp', []);
 
 app.controller('budgetCtrl', function($scope) {
     $scope.expenses = [];
-
+    
     $scope.addExpense = function(expense) {
       console.log(expense)
       $scope.expenses.push(expense);
+      $scope.expenseTotal = $scope.expenses.reduce((a,b) => a + b);
     }
 })
