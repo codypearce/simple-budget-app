0fbdc71c5b1e634a95b5c41749b124b3f190a056
Cody Pearce <codyapearce@gmail.com>
2017-04-27 21:19:32 -0600

    change expense to object and make amount a property

--- app/app.js
+++ app/app.js
@@ -2,14 +2,14 @@ var app = angular.module('budgetApp', []);
 
 app.controller('budgetCtrl', function($scope) {
     $scope.expenses = [];
-    $scope.addExpense = function(expense, type) {
+    $scope.addExpense = function(amount, type) {
       var obj = {
-        expense: expense,
+        amount: amount,
         type: type,
       };
       $scope.expenses.push(obj);
       $scope.expenseTotal = $scope.expenses.reduce((a,b) => a + b);
-      $scope.expense = '';
+      $scope.amount = '';
       $scope.type = '';
       console.log($scope.expenses);
     }
