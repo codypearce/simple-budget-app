934fc619ef20efbb415397bb3ad17b679dd51d60
Cody Pearce <codyapearce@gmail.com>
2017-04-27 21:16:46 -0600

    add type to array, expenses is now an array of objects

--- app/app.js
+++ app/app.js
@@ -2,11 +2,14 @@ var app = angular.module('budgetApp', []);
 
 app.controller('budgetCtrl', function($scope) {
     $scope.expenses = [];
-
-    $scope.addExpense = function(expense) {
-      console.log(expense)
-      $scope.expenses.push(expense);
+    $scope.addExpense = function(expense, type) {
+      var obj = {
+        expense: expense,
+        type: type,
+      };
+      $scope.expenses.push(obj);
       $scope.expenseTotal = $scope.expenses.reduce((a,b) => a + b);
-      $scope.expense = 0;
+      $scope.expense = '';
+      console.log($scope.expenses);
     }
 })
