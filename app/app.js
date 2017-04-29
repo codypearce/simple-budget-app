27464b45c1baff50b6c5362101a29da5a817e052
Cody Pearce <codyapearce@gmail.com>
2017-04-28 20:58:32 -0600

    sum up expense amount and output to expenses

--- app/app.js
+++ app/app.js
@@ -2,13 +2,15 @@ var app = angular.module('budgetApp', []);
 
 app.controller('budgetCtrl', function($scope) {
     $scope.expenses = [];
+    $scope.expenseTotal = 0;
+
     $scope.addExpense = function(amount, type) {
       var obj = {
         amount: amount,
         type: type,
       };
       $scope.expenses.push(obj);
-      $scope.expenseTotal = $scope.expenses.reduce((a,b) => a + b);
+      $scope.expenseTotal = $scope.expenses.reduce((a,b) => ({amount: a.amount + b.amount}));
       $scope.amount = '';
       $scope.type = '';
       console.log($scope.expenses);
