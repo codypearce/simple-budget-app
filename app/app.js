c8844836e4f376b0f603d0b91fa46a52eac38aa0
Cody Pearce <codyapearce@gmail.com>
2017-05-03 21:10:40 -0600

    add chart.js and angular-chart.js

--- app/app.js
+++ app/app.js
@@ -1,33 +1,35 @@
-var app = angular.module('budgetApp', []);
+var app = angular.module('budgetApp', ['chart.js']);
 
 app.controller('budgetCtrl', function($scope) {
-    $scope.expenses = [];
-    $scope.expenseTotal = 0;
+  $scope.expenses = [];
+  $scope.expenseTotal = 0;
 
-    $scope.addExpense = function(amount, type) {
-      if(!amount) {
-        return;
-      }
-      var obj = {
-        amount: amount,
-        type: type,
-        remove: false
-      };
-      $scope.expenses.push(obj);
+  $scope.addExpense = function(amount, type) {
+    if (!amount) {
+      return;
+    }
+    var obj = {
+      amount: amount,
+      type: type,
+      remove: false,
+    };
+    $scope.expenses.push(obj);
+    this.updateTotal();
+    $scope.amount = '';
+    $scope.type = '';
+  };
+  $scope.removeExpense = function(expense) {
+    expense.remove = true;
+    $scope.expenses = $scope.expenses.filter(a => a.remove == false);
+    if ($scope.expenses.length === 0) {
+      $scope.expenseTotal = 0;
+    } else {
       this.updateTotal();
-      $scope.amount = '';
-      $scope.type = '';
     }
-    $scope.removeExpense = function(expense) {
-      expense.remove = true;
-      $scope.expenses = $scope.expenses.filter(a => a.remove == false);
-      if($scope.expenses.length === 0) {
-        $scope.expenseTotal = 0;
-      } else {
-        this.updateTotal();
-      }
-    }
-    $scope.updateTotal = function() {
-      $scope.expenseTotal = $scope.expenses.reduce((a,b) => ({amount: a.amount + b.amount}));
-    }
-})
+  };
+  $scope.updateTotal = function() {
+    $scope.expenseTotal = $scope.expenses.reduce((a, b) => ({
+      amount: a.amount + b.amount,
+    }));
+  };
+});
