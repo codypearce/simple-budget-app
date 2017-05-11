0b893f523af2624a0fc232b21621f4436faf79a1
Cody Pearce <codyapearce@gmail.com>
2017-05-10 20:00:52 -0600

    create login page, fix header links

--- app/routes.js
+++ app/routes.js
@@ -1,29 +1,27 @@
 module.exports = function(app, passport) {
   app.get('/', function(req, res) {
     res.render('index');
-  })
+  });
   app.get('/login', function(req, res) {
-     res.render('login', { message: req.flash('loginMessage') });
- });
+    res.render('login', {message: req.flash('loginMessage')});
+  });
   app.get('/signup', function(req, res) {
-      res.render('signup', { message: req.flash('signupMessage') });
+    res.render('signup', {message: req.flash('signupMessage')});
   });
   app.get('/profile', isLoggedIn, function(req, res) {
-       res.render('profile', {
-           user : req.user
-       });
-   });
-   app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
+    res.render('profile', {
+      user: req.user,
     });
-}
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
+  });
+  app.get('/logout', function(req, res) {
+    req.logout();
     res.redirect('/');
+  });
+};
+function isLoggedIn(req, res, next) {
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
 }
