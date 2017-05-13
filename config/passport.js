a76dcff53520d72ccd6dc16e45d95e6e08af48d1
Cody Pearce <codyapearce@gmail.com>
2017-05-13 14:08:14 -0600

    add config fodler and create passport sign up and login functions

---
+++ config/passport.js
@@ -0,0 +1,48 @@
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('../app/models/user');
+
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserialzeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(
+    'local-signup',
+    new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true,
+    })
+  ), function(req, email, password, done) {
+    process.nextTick(function() {
+      User.findOne({'local.email': email}, function(err, user) {
+        if (err) return done(err);
+
+        if (user) {
+          return done(
+            null,
+            false,
+            req.flash('signupMessage', 'That Email is already taken.')
+          );
+        } else {
+          var newUser = newUser();
+
+          newUser.local.email = email;
+          newUser.local.password = newUser.generateHash(password);
+
+          newUSer.save(function(err) {
+            if (err) throw err;
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  };
+};
