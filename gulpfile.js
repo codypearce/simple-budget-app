2ec882311ce87538c64ba95e411079a235654216
codypearce <codyapearce@gmail.com>
2017-05-18 20:37:12 -0600

    npm script for dev and start, fix .tmp folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ const gulp = require('gulp'),
       stylus = require('gulp-stylus'),
       concat = require('gulp-concat');
 
-
 gulp.task('css', function() {
-	return gulp.src('./client/styles/*.css')
+	return gulp.src('./client/styles/*.styl')
     .pipe(stylus())
     .pipe(concat('styles.css'))
-    .pipe(gulp.dest('.temp'))
+    .pipe(gulp.dest('.tmp'))
 });
